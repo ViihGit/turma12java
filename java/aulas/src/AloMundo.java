b605f30948bb6a27e4085f936c28d66301f2821a
ViihGit <vitoriagcf@hotmail.com>
2020-11-07 16:35:26 -0300

    Exercicios em Java

---
+++ java/aulas/src/AloMundo.java
@@ -0,0 +1,16 @@
+
+public class AloMundo {
+	
+	public static void main(String[]args){
+		//Commentary
+			/* 
+			 * 
+			 */
+		System.out.print("Hello World - Olá mundooooo!!!!");
+		
+		
+		
+		
+	}
+		
+}
---
+++ java/aulas/src/meuNome.java
@@ -0,0 +1,12 @@
+
+public class meuNome {
+		public static void main (String[]args) {
+			int anoNascimento = 1998;
+			int idade =0;
+			double salario = 600;
+			String nome = "Vitória Gonçalves"; 
+			idade = 2020 - 1998;
+			System.out.printf("\nMeu nome é %s, nascido em %d e minha idade é %d e meu salário é %.2f", nome , anoNascimento, idade, salario); 	
+		
+		}
+}
