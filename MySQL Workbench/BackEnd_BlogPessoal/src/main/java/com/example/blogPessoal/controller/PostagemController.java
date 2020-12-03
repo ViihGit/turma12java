20c8244057c03d9ab4003f6ae11d3ef283b99e61
VitoriaAlric <vitoriacf@hotmail.com>
2020-12-03 09:49:45 -0300

    STS + Postman

--- MySQL Workbench/BackEnd_BlogPessoal/src/main/java/com/example/blogPessoal/controller/PostagemController.java
+++ MySQL Workbench/BackEnd_BlogPessoal/src/main/java/com/example/blogPessoal/controller/PostagemController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.blogPessoal.model.Postagem;
@@ -20,7 +25,7 @@ public class PostagemController {
 
 	@Autowired				 //Injeção de independencias do Spring, garantimos que todos os serviços desta interface, do tipo Repository sejam aceitadas apartir do Controller
 	private PostagemRepository repository;
-	@GetMapping             //Sempre que vier uma requisição externa ele disparará o método abaixo
+	@GetMapping            	 //Sempre que vier uma requisição externa ele disparará o método abaixo
 	public ResponseEntity<List<Postagem>> GetAll () {
 		return ResponseEntity.ok(repository.findAll());
 	}
@@ -28,9 +33,30 @@ public class PostagemController {
 	public Optional <Postagem> encontrarUm (@PathVariable long id){
 		return repository.findById(id);
 		}
-	
-	
 
+    //___________________________________________________________Inserir POST___________________________________________________________________________________________________________________________________________________________________________________________________
+	//______________________________________________________localhost:8080/postagem___________________________________________________________________________________________________________________________________________________________________________________________________________
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	//_____________________________________________________________Updates PUT_______________________________________________________________________________________________________________________________________________________________________________________________________________________
+	//_____________________________________________________Para Put passamos o Id do recurso que queremos alterar____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+	//Retornara um objeto do tipo ResponseEntity, com o recurso Postagem dentro dele, receberemos como parametro um objeto via @RequestBody postagem, Receberemos ResponseEntity.status com status de ok  e detro teremos o metodo save  
+	
+	@PutMapping 				
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	//_____________________________________________________________Deletar DELETE_________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+	//Fizemos um metodo void, ou seja, ele não retorna nada, utilizamos um PathVariael, capturamos o Id dentro da variave long, chamamos o repository com o metodo DeleteById, e em delete, não passamos o return.
+	
+	@DeleteMapping("/{id}")		
+	public void delete (@PathVariable long id ) {
+		repository.deleteById(id);
+	}
 
 
 
